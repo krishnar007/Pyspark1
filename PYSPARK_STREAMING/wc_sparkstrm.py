dd026b7767bd00676353c0ab3e43ab9efc0fe065
krishnar007 <155872524+krishnar007@users.noreply.github.com>
2024-01-07 15:46:25 +0530

    Update wc_sparkstrm.py
    updated

--- PYSPARK_STREAMING/wc_sparkstrm.py
+++ PYSPARK_STREAMING/wc_sparkstrm.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     sc = SparkContext(appName="PySpark Streaming", master="local[2]")
     ssc = StreamingContext(sc, 5)
     #source_path = sys.argv[1]
-    # source_path = u"/Users/krishnar/PycharmProjects/SPARKSQL-TEST/PYSPARK_STREAMING/data/word.txt"
-    # dstream = ssc.textFileStream("file:///Users/krishnar/PycharmProjects/SPARKSQL-TEST/PYSPARK_STREAMING/data/")
+    # source_path = u"/Users/PycharmProjects/SPARKSQL-TEST/PYSPARK_STREAMING/data/word.txt"
+    # dstream = ssc.textFileStream("file:///Users/PycharmProjects/SPARKSQL-TEST/PYSPARK_STREAMING/data/")
     dstream = ssc.socketTextStream('localhost', 9999)
     words = dstream.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda x, y: x + y)
     words.pprint()
